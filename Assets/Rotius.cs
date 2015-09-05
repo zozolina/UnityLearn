d53f498b44f34447f64080b5a939125cf66347be
zozolina <mihaela.avadanesei@gmail.com>
2015-09-05 19:09:38 +0300

    Init
    First Commit

---
+++ Assets/Rotius.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotius : MonoBehaviour 
+{
+
+
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+		transform.Rotate (Vector3.up);
+	}
+}
